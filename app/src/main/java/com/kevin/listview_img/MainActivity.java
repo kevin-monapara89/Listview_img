92a76aa5cb6e8121880f37b46865c9ed8c509bce
kevin monapara <kevinmonapara89@gmail.com>
2023-04-02 21:16:51 +0530

    commit

--- app/src/main/java/com/kevin/listview_img/MainActivity.java
+++ app/src/main/java/com/kevin/listview_img/MainActivity.java
@@ -1,5 +1,8 @@
 package com.kevin.listview_img;
 
+
+
+
 import static com.kevin.listview_img.R.drawable.*;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listtxt;
 
-    int image[] = {R.drawable.img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8,img_9,img_10,img_11,img_12,img_13,img_14,img_15,};
+    int image[] = {img_1,img_2,img_3,img_4,img_5,img_6,img_7,img_8,img_9,img_10,img_11,img_12,img_13,img_14,img_15,
+    img_16,img_17,img_18,img_19,img_20,img_21,img_22,img_23,img_24,img_25,img_26,img_27,img_28,img_29,img_30};
 
-    String name[] = {"Surat","Ahmedabad","Vadodara","Rajkot","Mumbai","Pune","Chennai","Bangalore","Hyderabad","Kolkata","Delhi","Jaipur","Lucknow","Kanpur","Nagpur"};
+    String name[] = {"Surat","Ahmedabad","Vadodara","Rajkot","Mumbai","Pune","Chennai","Bangalore","Hyderabad","Kolkata","Delhi","Jaipur","Lucknow","Kanpur","Nagpur",
+                     "Indore","Thane","Bhopal","Visakhapatnam","Patna","Ludhiana","Agra","Nashik","Varanasi","Amritsar","Srinagar","Navi mumbai","Ranchi","Jodhpur","Gandhinagar"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
